733f27adc8692b60ec9418fb2293f6b721de0efd
rksorry7255tu <rk7255053708@gmail.com>
2024-08-26 21:12:25 +0530

    finding nth largesr no from reverse in shorted array of integer

---
+++ kthlargestno.java
@@ -0,0 +1,37 @@
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Can you solve it without sorting?
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+// Example 2:
+
+// Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+// Output: 4
+public class kthlargestno {
+    public static void main(String[]args){
+        int nums[]={3,2,3,1,2,4,5,5,6};
+        int k=4;
+        int kthno=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    int temp =nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        System.out.println(nums[nums.length-k]);
+        
+        // for(int i=0;i<nums.length;i++){
+        //     System.out.println(nums[i]);
+        // }
+    }
+}

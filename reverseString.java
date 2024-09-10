8229d2549be791155da281a0e7050cc9c47d010e
rksorry7255tu <rk7255053708@gmail.com>
2024-09-11 05:13:16 +0530

    reverse string

--- reverseString.java
+++ reverseString.java
@@ -1,10 +1,17 @@
 public class reverseString {
     public static void main(String[] args) {
-        StringBuilder name=new StringBuilder("Ravi Kumar");
-        StringBuilder namerev=new StringBuilder();
-        for(int i=name.length()-1;i>=0;i--){
-           namerev.append(name.charAt(i));
+        StringBuilder name=new StringBuilder("Ravi");
+        
+        //System.out.println(name);
+        for(int i=0;i<name.length()/2;i++){
+            int front=i;
+            int back=name.length()-1-i;
+            char frontchar=name.charAt(front);
+            char backchar=name.charAt(back);
+            name.setCharAt(front,backchar);
+            name.setCharAt(back,frontchar);
         }
-        System.out.println(namerev);
+        name.insert(4,"Kumar");
+        System.out.println(name);
     }
 }

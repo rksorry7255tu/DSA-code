4b5baa65c2d2346e40e8361f8323322edbd249f1
rksorry7255tu <rk7255053708@gmail.com>
2024-09-22 12:49:28 +0530

    optimized solution

---
+++ slidingwindow9.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class slidingwindow9 {
+    public static void main(String[] args) {
+        String s="aaabbccd";
+        int k=2;
+        HashMap<Character,Integer>map=new HashMap<>();
+        int l=0; int r=0; int maxlength=0;
+
+        while(r<s.length()){
+            if(!map.containsKey(s.charAt(r))){
+                map.put(s.charAt(r),1);
+            }else{
+                map.put(s.charAt(r),map.get(s.charAt(r))+1);
+            } 
+            if(map.size()>2){ 
+                if(map.size()>2){
+                    map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                    if(map.get(s.charAt(l))==0){
+                        map.remove(s.charAt(l));
+                    }
+                    l++;
+                }
+            }
+            maxlength=Math.max(maxlength,r-l+1);
+            r++;
+        }
+        System.out.println(maxlength);
+    }
+}

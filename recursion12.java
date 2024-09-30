be4875ad093f067ac72796a786ff526908d345a9
rksorry7255tu <rk7255053708@gmail.com>
2024-10-01 00:50:24 +0530

    solved

---
+++ recursion12.java
@@ -0,0 +1,22 @@
+//fibonacci no
+
+public class recursion12 {
+    public static void main(String[] args) {
+        int n=10;
+       for(int i=0;i<n;i++){
+            System.out.print(fib(i)+" ");
+       }
+
+      
+    }
+    public static int fib(int n){
+       if(n<=1){
+       return n;
+       }else{
+        return fib(n-1)+fib(n-2);
+       }
+
+      
+        
+    }
+}
